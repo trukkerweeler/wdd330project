e4c0e7dfe4494fc5c5b601a5b52793b6a0794efd
trukkerweeler <ken22008@byui.edu>
2023-06-02 08:25:34 -0600

    0602-0825_vite-rebuild

--- src/js/main.js
+++ src/js/main.js
@@ -5,6 +5,6 @@ import { loadHeaderFooter } from "./utils.mjs";
 // });
 
 loadHeaderFooter();
-const date = document.getElementById("date");
-// console.log(date);
-date.innerHTML = `${new Date(Date.now()).getFullYear()}  Auto mXr is NOT a real business!`;
+// const date = document.getElementById("date");
+// // console.log(date);
+// date.innerHTML = `${new Date(Date.now()).getFullYear()}  Auto mXr is NOT a real business!`;
